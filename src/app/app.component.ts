0f0e2f4b18794703ed7d2e0ef976841f099174a0
J0hnNeves <joaonvsjvn@gmail.com>
2024-05-14 20:22:33 -0300

    segundo commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LojaPage } from './loja/loja.page';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor() {}
+}
+
